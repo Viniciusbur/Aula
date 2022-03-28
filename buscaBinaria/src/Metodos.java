6f12102116e62211099e4b00797584587e7019ae
Vinicius Burgie Josina <81188849+Viniciusbur@users.noreply.github.com>
2022-03-28 16:45:06 -0300

    Add files via upload

--- buscaBinaria/src/Metodos.java
+++ buscaBinaria/src/Metodos.java
@@ -2,6 +2,16 @@ import java.util.Random;
 
 public class Metodos {
 
+    private long tempoIni = 0, tempoFim = 0;
+
+    public long getTempoIni() {
+        return tempoIni;
+    }
+
+    public long getTempoFim() {
+        return tempoFim;
+    }
+
     public int[] geraVetor(int nroElem, int lim) {
         int dummy;
         int[] res = null;
@@ -21,10 +31,10 @@ public class Metodos {
         return res;
     }
 
-    public String imprime(int vetor[]){
+    public String toString(int vetor[]){
         String imprime = "";
         for(int i = 0; i<vetor.length;i++){
-             imprime += " " + vetor[i];
+             imprime += "\n" + vetor[i];
         }
 
         return imprime;
@@ -33,10 +43,67 @@ public class Metodos {
     int pesqDireta(int [] vet, int valor) {
         int res = -1, i;
         for (i = 0; ((i < vet.length) && (vet[i] != valor)); i++);
-        if (i < vet.length)
+        if (i < vet.length){
             res = i;
-        return res;
+        return res;}
+        else  return -5;
     }
 
 
-}
+
+    public void initClock() {
+        tempoIni = tempoFim = System.nanoTime();
+    }
+
+    public double getClockSec() {
+        double res;
+
+        tempoFim = System.nanoTime();
+
+        res =  ((double)tempoFim - (double)tempoIni) / (double)1_000_000_000.0;
+
+        return res;
+    }
+
+    public void ordenaVetor(int num[], int tam) {
+        int min, max, aux;
+        for (int i = 0; i < tam - 1; i++) {
+            min = i;
+            for (int j = (i + 1); j < tam; j++) {
+                if (num[j] < num[min]) {
+                    min = j;
+                }
+            }
+            if (i != min) {
+                aux = num[i];
+                num[i] = num[min];
+                num[min] = aux;
+            }
+        }
+    }
+        public int binarySearch(int num[], int value){
+            int low = 0;
+            int high = num.length - 1;
+            int p = low + ((high-low)/2);
+            while (low <= high)
+            {
+                if (num[p] > value) {
+                    high = p - 1;
+                }else if (num[p] < value) {
+                    low = p + 1;
+                }else{
+                 p = low + ((high-low)/2);
+                return p;
+
+                }
+            }
+            return -1;
+        }
+
+
+    }
+
+
+
+
+
--- buscaBinaria/src/Principal.java
+++ buscaBinaria/src/Principal.java
@@ -1,21 +1,23 @@
-
+import java.util.Arrays;
 
 public class Principal {
 
     public static void main(String[] args) {
 
     int []vetor = null;
-
-
+    int tamanhoVetor = 10 ;
     Metodos metodos = new Metodos();
 
-    vetor = new int[100];
+    vetor = new int[tamanhoVetor];
+    vetor = metodos.geraVetor(tamanhoVetor, 1000);
 
-    vetor = metodos.geraVetor(100, 100);
+    Arrays.sort(vetor);
 
-    System.out.println(metodos.imprime(vetor));
+    System.out.println(metodos.toString(vetor));
+    System.out.println(metodos.getClockSec());
+    System.out.println(metodos.pesqDireta(vetor, 50));
+    System.out.println(metodos.binarySearch(vetor, 50));
+
 
     }
-
-
 }
